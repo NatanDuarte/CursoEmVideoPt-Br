c3a6dcaedbc33432de263b35e92cea2553835dd0
Natan Duarte <natanprocopio@gmail.com>
2018-12-26 10:33:59 -0200

    Add exercice 6

---
+++ ex006.py
@@ -0,0 +1,4 @@
+#leia um número e mostre o dobro o triplo e a raiz quadrada
+
+n = int(input('digite um número: '))
+print(' o dobro de >{}< é {}\n o triplo é {}\n e sua raiz quadrada é {:.1f}'.format(n, n*2, n*3, n**(1/2)))
