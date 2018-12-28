bde84800be44a6770038d2a23f0488c7e06bf725
Natan Duarte Procópio <44822508+NatanDuarte@users.noreply.github.com>
2018-12-27 23:01:35 -0200

    Delete ex005.py

--- ex005.py
+++
@@ -1,4 +0,0 @@
-# criar um programa capaz  de ler um número inteiro e mostrar seu antecessor e sucessor
-
-n = int(input('Digite um número: '))
-print('{} é o antecessor de {} e {} é o seu sucessor'.format(n-1, n, n+1))
