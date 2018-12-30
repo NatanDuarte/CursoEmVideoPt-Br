f5a5d368155e859561584be7cbce38d5eb8ae53b
Natan Duarte <natanprocopio@gmail.com>
2018-12-30 11:16:34 -0200

    Exercice 13 added

---
+++ ExerciceList/ex013.py
@@ -0,0 +1,4 @@
+nome = input('Quem vai receber o aumento? ')
+s = int(input('Quanto ele(a) ganha? '))
+aum = (s*15)/100
+print('com 15% de aumento, o novo salario de {} é de {} reais'.format(nome, s+aum))
