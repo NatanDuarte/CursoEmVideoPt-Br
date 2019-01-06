e00148757cb3575aa40e0a66a53eb8355bcdd64c
Natan Duarte <natanprocopio@gmail.com>
2019-01-06 14:22:29 -0200

    Exercice 27 added

---
+++ ExerciceList/ex027.py
@@ -0,0 +1,6 @@
+"""Exercício Python 027: Faça um programa que leia o nome completo de uma
+pessoa, mostrando em seguida o primeiro e o último nome separadamente."""
+
+nome = str(input('Digite seu nome completo: ').strip()).split()
+print('Primeiro nome: {}'.format(nome[0]))
+print('Ultimo nome: {}'.format(nome[len(nome)-1]))
